a67b9f484011a527ce66b7764eded98ed934eb0c
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-31 03:13:30 +0700

    Transaksi Modul

---
+++ app/Http/Controllers/api/v1/TransaksiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TransaksiController extends Controller
+{
+    // SELECT * FROM history_transaksi
+    // WHERE cracc='0010101201041458' NO_TABUNGAN
+    // AND tgltrn BETWEEN '20231101' AND '20231231'
+}
