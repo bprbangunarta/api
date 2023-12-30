5dc8fd7bb7464015aeeac6135568c48295a753f1
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-31 04:51:30 +0700

    Transaksi Modul

--- app/Http/Controllers/api/v1/TransaksiController.php
+++ app/Http/Controllers/api/v1/TransaksiController.php
@@ -6,10 +6,40 @@ use App\Http\Controllers\Controller;
 use App\Models\Tabungan;
 use App\Models\Transaksi;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class TransaksiController extends Controller
 {
+
+    public function index($id)
+    {
+        $now = Carbon::now();
+        $tahun = $now->format('m');
+        $bulan = $now->format('Y');
+        $param = $tahun . $bulan;
+
+        $transaksi = Transaksi::where(function ($query) use ($id) {
+            $query->where('cracc', $id)
+                ->orWhere('dracc', $id);
+        })
+            ->where('thnbln', $param)
+            ->orderBy('inptgljam', 'ASC')
+            ->get();
+
+        if ($transaksi->isNotEmpty()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Success',
+                'data'    => $transaksi
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Failed',
+                'data'    => ''
+            ], 401);
+        }
+    }
 
     public function by_date($id, $startDate, $endDate)
     {
