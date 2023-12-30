d3ccc9befd36c028f0bd9cb7885f53e84f72b685
Zulfadli Rizal <dev@bprbangunarta.co.id>
2023-12-31 04:23:54 +0700

    Transaksi Modul

--- app/Http/Controllers/api/v1/TransaksiController.php
+++ app/Http/Controllers/api/v1/TransaksiController.php
@@ -7,7 +7,9 @@ use Illuminate\Http\Request;
 
 class TransaksiController extends Controller
 {
-    // SELECT * FROM history_transaksi
-    // WHERE cracc='0010101201041458' NO_TABUNGAN
-    // AND tgltrn BETWEEN '20231101' AND '20231231'
+    // SELECT *
+    // FROM history_transaksi
+    // WHERE (cracc = '0010101201041458' OR dracc = '0010101201041458')
+    // AND tgltrn BETWEEN '20231130' AND '20231231'
+    // ORDER BY inptgljam ASC;
 }
